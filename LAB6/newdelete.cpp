72c238cd04d587114cf6cd0832d28fb01339561b
Vivek Sharma <161102201+geekovic@users.noreply.github.com>
2024-12-14 14:22:30 +0530

    Update newdelete.cpp

--- LAB6/newdelete.cpp
+++ LAB6/newdelete.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
+using namespace std;
+
 class Distance{
  private:
  int m_feet;
  int m_inches;
+
  public:
  Distance(): m_feet(0), m_inches(0) {}
  Distance(int feet, int inches): m_feet(feet), m_inches(inches) {}
+
  void getDistance()
  {
- std::cout << "Enter feet: ";
- std::cin >> m_feet;
- std::cout << "Enter inches: ";
- std::cin >> m_inches;
- printLines();
+    cout << "Enter feet: ";
+    cin >> m_feet;
+    cout << "Enter inches: ";
+    cin >> m_inches;
+    printLines();
  }
  void showDistance()
  {
- std::cout << "Feet => " << m_feet << " | Inches => " << m_inches << std::endl;
- printLines();
+    cout << "Feet => " << m_feet << " | Inches => " << m_inches << endl;
+    printLines();
  }
  void printLines()
  {
- for (int i = 0; i <= 30; i++)
- std::cout << "-";
- std::cout << std::endl;
+    for (int i = 0; i <= 30; i++)
+        cout << "-";
+        cout << endl;
  }
 };
+
 int main()
 {
- Distance* ptr_d1 = new Distance(8,9);
- Distance* ptr_d2 = new Distance();
- ptr_d1->showDistance();
- ptr_d2->getDistance();
- ptr_d2->showDistance();
+    Distance* ptr_d1 = new Distance(8,9);
+    Distance* ptr_d2 = new Distance();
+    ptr_d1->showDistance();
+    ptr_d2->getDistance();
+    ptr_d2->showDistance();
 }
